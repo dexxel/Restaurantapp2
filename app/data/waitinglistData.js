933258e34cd6c46450977bc7da22ff4179bd18ef
dexxel <bakilin.alexandr@gmail.com>
2018-08-25 11:57:53 -0700

    <new file>

---
+++ app/data/waitinglistData.js
@@ -0,0 +1,20 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the waitlist tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var waitingArray = [
+    {
+      customerName: "Dasha",
+      customerEmail: "dasha@example.com",
+      customerID: "dashacool",
+      phoneNumber: "000-000-0000"
+      
+    }
+  ];
+  
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = waitingArray;
